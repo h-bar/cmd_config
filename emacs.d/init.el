6f639850325f002aea9b0f09099cb27e8eef26d7
Yan Zhang <Yan Zhang>
2020-10-08 22:32:25 -0400

    use straight for emacs packages

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,124 +1,53 @@
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(package-initialize)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (linum-relative company-lsp company-ctags ivy-xref counsel-gtags counsel-projectile lsp-ivy ivy-rich ripgrep counsel lsp-ui magit flycheck))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-(require 'use-package)
-(setq use-package-always-ensure t)
-
-(use-package flycheck
-  :config
-  (global-flycheck-mode))
-
-(use-package magit)
-
-(use-package ivy
-  :config
-  (ivy-mode t)
-  (setq ivy-use-virtual-buffers t)
-  (setq enable-recursive-minibuffers t)
-  (setq ivy-count-format "")
-  ;; Do not show "./" and "../" in the `counsel-find-file' completion list
-  (setq ivy-extra-directories nil)    ;Default value: ("../" "./")
-  :bind(
-	("C-s" . counsel-grep-or-swiper)
-	("C-x b" . ivy-switch-buffer)
-	))
-
-(use-package company-ctags
-  :after company
-  :config
-  (company-ctags-auto-setup)
-  (setq company-ctags-support-etags 1)
-  (setq company-ctags-fuzzy-match-p 1))
-
-(use-package company-lsp
-  :after company
-  :config
-  (push 'company-lsp company-backends))
-
-(use-package counsel
-  :after ivy
-  :config
-  (counsel-mode t)
-  :bind(
-	("C-c b" . counsel-bookmark)
-	))
-
-(use-package counsel-projectile
-  :after counsel)
-
-(use-package swiper)
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
 
-(use-package rainbow-delimiters
-  :config
-  (rainbow-delimiters-mode-enable))
 
-(use-package projectile)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Install Packages
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(straight-use-package 'rainbow-delimiters)
+(straight-use-package 'smartparens)
 
-(use-package lsp-mode
-  :config
-  (lsp-mode t))
+(straight-use-package 'ivy)
+(straight-use-package 'counsel)
+(straight-use-package 'swiper)
+(straight-use-package 'projectile)
+(straight-use-package 'company)
 
-(use-package lsp-ui
-  :config
-  (lsp-ui-mode t))
+(straight-use-package 'ivy-rich)
 
-(use-package smartparens
-  :config
-  (smartparens-global-mode t))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Config Packages
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq ivy-rich-path-style 'abbrev)
 
-(use-package company
-  :config
-  (global-company-mode t))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Enable Modes
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(use-package ripgrep)
+;; Modes from Installed Packages
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+(smartparens-global-mode t)
 
-(use-package ivy-rich
-  :after (ivy)
-  :config
-  (ivy-rich-mode t)
-  (setq ivy-rich-path-style 'abbrev))
+(ivy-mode t)
+(counsel-mode t)
+(global-company-mode t)
+(ivy-rich-mode t)
 
-(use-package lsp-ivy)
-(use-package counsel-gtags)
-
-(use-package ivy-xref
-  :init
-  ;; xref initialization is different in Emacs 27 - there are two different
-  ;; variables which can be set rather than just one
-  (when (>= emacs-major-version 27)
-    (setq xref-show-definitions-function #'ivy-xref-show-defs))
-  ;; Necessary in Emacs <27. In Emacs 27 it will affect all xref-based
-  ;; commands other than xref-find-definitions (e.g. project-find-regexp)
-  ;; as well
-  (setq xref-show-xrefs-function #'ivy-xref-show-xrefs))
-
-(use-package linum-relative
-  :config
-  (linum-relative-global-mode t))
-
+;; Builtin Modes
 (global-cwarn-mode t)
-;;(global-display-line-numbers-mode t)
 (global-prettify-symbols-mode t)
 (global-visual-line-mode t)
+(global-display-line-numbers-mode t)
 (xterm-mouse-mode t)
