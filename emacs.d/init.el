8e49af75224627c1584634b41cd40435361101e8
Yan Zhang <663yan@gmail.com>
2020-10-20 12:12:57 -0400

    update config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -11,91 +11,119 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
+
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Install Packages
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(straight-use-package 'rainbow-delimiters)
-(straight-use-package 'smartparens)
-(straight-use-package 'ripgrep)
-(straight-use-package 'evil-nerd-commenter)
-(straight-use-package 'dtrt-indent)
+(use-package smartparens
+  :init
+  (require 'smartparens-config)
+  :config
+  (smartparens-global-mode t))
 
-(straight-use-package 'all-the-icons)
-(straight-use-package 'doom-modeline)
+;; (use-package solarized-theme
+;;   :config
+;;   (load-theme 'solarized-dark t))
 
-(straight-use-package 'ivy)
-(straight-use-package 'counsel)
-(straight-use-package 'swiper)
-(straight-use-package 'projectile)
-(straight-use-package 'company)
-(straight-use-package 'hydra)
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
 
-(straight-use-package 'ivy-hydra)
-(straight-use-package 'ivy-rich)
-(straight-use-package 'ivy-xref)
-(straight-use-package 'counsel-projectile)
+(use-package ripgrep)
+(use-package evil-nerd-commenter
+  :config
+  (evilnc-default-hotkeys t))
+
+(use-package doom-modeline
+  :init (doom-modeline-mode t)
+  :config
+  (setq doom-modeline-window-width-limit fill-column)
+  (setq doom-modeline-project-detection 'projectile)
+  (setq doom-modeline-buffer-file-name-style 'file-name)
+  (setq doom-modeline-indent-info t))
+
+(use-package ivy
+  :config
+  (setq ivy-use-virtual-buffers t)
+  (ivy-mode t))
+(use-package counsel
+  :config
+  (counsel-mode t))
+(use-package swiper)
+(use-package hydra)
+
+(use-package flycheck
+  :hook (prog-mode . flycheck-mode))
+
+(use-package projectile
+  :config
+  (setq projectile-completion-system 'ivy)
+  (setq projectile-dynamic-mode-line t)
+  (setq projectile-indexing-method 'alien)
+  (setq projectile-sort-order 'recently-active)
+  (setq projectile-enable-caching t)
+  (setq projectile-mode-line-function '(lambda () (format " Proj[%s]" (projectile-project-name))))
+  (projectile-mode t))
+
+(use-package company
+  :config
+  (global-company-mode t))
+
+(use-package lsp-ui
+  :config
+  (lsp-ui-peek-enable t)
+  (lsp-ui-mode t)
+  (lsp-ui-sideline-mode t))
+
+(use-package lsp-mode
+  :config
+  (lsp-completion-mode t)
+  (lsp-diagnostics-mode t)
+  (lsp-diagnostics-modeline-mode t))
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Config Packages
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Ivy, Counsel and Swiper
-(setq ivy-use-virtual-buffers t)
-(setq ivy-rich-path-style 'abbrev)
 
-;; Projectile
-(setq projectile-completion-system 'ivy)
-(setq projectile-dynamic-mode-line t)
-(setq projectile-indexing-method 'alien)
-(setq projectile-sort-order 'recently-active)
-(setq projectile-enable-caching t)
-;; (setq projectile-mode-line-function '(lambda () (format " Proj[%s]" (projectile-project-name))))
+(use-package ivy-hydra)
 
-;; Modeline
-(setq doom-modeline-window-width-limit fill-column)
-(setq doom-modeline-project-detection 'projectile)
-(setq doom-modeline-buffer-file-name-style 'file-name)
-(setq doom-modeline-indent-info t)
+(use-package ivy-rich
+  :config
+  (setq ivy-rich-path-style 'abbrev)
+  (ivy-rich-mode))
 
-;; Ivy-xref
-(when (>= emacs-major-version 27)
-  (setq xref-show-definitions-function #'ivy-xref-show-defs))
-(setq xref-show-xrefs-function #'ivy-xref-show-xrefs)
+(use-package ivy-xref
+  :config
+  (when (>= emacs-major-version 27)
+    (setq xref-show-definitions-function #'ivy-xref-show-defs))
+  (setq xref-show-xrefs-function #'ivy-xref-show-xrefs))
 
+(use-package counsel-projectile)
+
+(use-package company-lsp
+  :config
+  (push 'conpany-lsp company-backends))
+
+;; (use-package company-box
+;;   :hook (company-mode . company-box-mode))
+
+;; (use-package flycheck-color-mode-line
+;;   :hook (flycheck-mode . flycheck-color-mode-line-mode))
 
 (load (concat user-emacs-directory "comphy_fw"))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Enable Modes
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; Modes from Installed Packages
-(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
-(smartparens-global-mode t)
-
-(ivy-mode t)
-(counsel-mode t)
-(global-company-mode t)
-(ivy-rich-mode t)
-(doom-modeline-mode t)
-(projectile-mode t)
-
-;; Builtin Modes
 (global-cwarn-mode t)
 (global-prettify-symbols-mode t)
 (global-visual-line-mode t)
 (global-display-line-numbers-mode t)
 (xterm-mouse-mode t)
-(which-func-mode t)
+(which-function-mode t)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (auto-save-visited-mode t)
 (display-time-mode t)
 (auto-save-mode nil)
+(global-hl-line-mode t)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; EMACS Configuration
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (setq scroll-conservatively most-positive-fixnum)
 (setq make-backup-files nil)
 (setq ring-bell-function 'ignore)
@@ -104,7 +132,8 @@
 ;; Keymaps
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (global-set-key "\C-s" 'swiper)
-(global-set-key "\M-;" 'evilnc-comment-or-uncomment-lines)
+;; (global-set-key "\C-x b" 'split-window-horizontally)
+;; (global-set-key "\C-x \\" 'split-window-vertically')
 
 (global-set-key (kbd "M-p g") 'counsel-projectile-rg)
 (global-set-key (kbd "M-p t") 'projectile-find-tag)
